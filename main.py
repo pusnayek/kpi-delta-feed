c3d2e7fd608055bb6faa37d6cc250d08eb44dc72
Puspendu Nayek1 <puspendu.nayek@gmail.com>
2023-11-21 12:52:42 +0530

    Replications done

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import sys
 
 def execute():
     files = []
-    sys.stdout.write("\n-----EXECUTION SUMMARY-----\n")
+    sys.stdout.write("\nINFO - EXECUTION SUMMARY-----\n")
     for type, filename, dataframe in sftp.read_files():
-        str = "File type - {type}, name - {name}, length - {len}".format(type = type, name = filename, len = len(dataframe.index))
+        str = "\nINFO - File type - {type}, name - {name}, length - {len}\n".format(type = type, name = filename, len = len(dataframe.index))
         sys.stdout.write(str)
         
         str = dbconnect.write_df_to_hana(dataframe, type)
         str = dbconnect.replicate(type)
-        sys.stdout.write("Replication done for {type}".format(type = type))
+        sys.stdout.write("\nINFO - Replication done for {type}\n".format(type = type))
         files.append(filename)
 
     for filename in files:        
         sftp.move_file(filename)
-        sys.stdout.write("File archived {filename}".format(filename = filename))
-
+        sys.stdout.write("\nINFO - File archived {filename}\n".format(filename = filename))
 
 def check_sftp():
     return sftp.check()
